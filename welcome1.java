2ef242b93a9e47db5ed884d9e625743fd18db8c7
howerz <92705176+howerz@users.noreply.github.com>
2022-01-26 18:30:42 -0500

    commit

---
+++ welcome1.java
@@ -0,0 +1,13 @@
+// Fig. 2.3:  Welcome1.java
+// Printing a line of text with multiple statements
+
+public class Welcome1
+{
+    //main method begins execution of Java application
+    public static void main(String[] args)
+    {
+       
+        System.out.println("Welcome to Java Programming!");
+   
+    } //end method main
+} //end class Welcome
