b71ec9b92ab7b9cc1105a891e808ef9214ba9339
howerz <92705176+howerz@users.noreply.github.com>
2022-01-26 19:14:21 -0500

    java lab2

---
+++ Lab2.java
@@ -0,0 +1,11 @@
+//Zach Hower
+//2022-01-26
+//Displays a 3 by 3 array of 1-9
+
+public class Lab2 {
+    public static void main(String[] args) { //Create method main
+        System.out.println("1 2 3"); // Display the string
+        System.out.print("4 5 6\n"); // Display string then new line
+        System.out.printf("%s", "7 8 9"); // format args as string, display string
+    } // end method main
+} //end class Lab2
