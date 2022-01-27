441bfe36a9022993211f1f1473aaff64676e58d2
howerz <92705176+howerz@users.noreply.github.com>
2022-01-26 19:30:03 -0500

    Restructure

---
+++ 2022 Spring .Net Framework/Lab1 - Zach Hower .Net Developement Web3/Lab1 - Zach Hower .Net Developement Web3/Form1.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnClick = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnClick
+        '
+        Me.btnClick.Location = New System.Drawing.Point(93, 72)
+        Me.btnClick.Name = "btnClick"
+        Me.btnClick.Size = New System.Drawing.Size(93, 43)
+        Me.btnClick.TabIndex = 0
+        Me.btnClick.Text = "Click me"
+        Me.btnClick.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 191)
+        Me.Controls.Add(Me.btnClick)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.Name = "Form1"
+        Me.Text = "Zach Hower - Lab 1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnClick As Button
+End Class
---
+++ 2022 Spring .Net Framework/Lab1 - Zach Hower .Net Developement Web3/Lab1 - Zach Hower .Net Developement Web3/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub btnClick_Click(sender As Object, e As EventArgs) Handles btnClick.Click
+        btnClick.Text = "Clicked"
+    End Sub
+End Class
