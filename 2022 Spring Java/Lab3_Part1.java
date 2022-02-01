1d96136ed65c42fb77ab2038e9858442f3e069e7
howerz <92705176+howerz@users.noreply.github.com>
2022-01-31 23:01:32 -0500

    Comments

--- 2022 Spring Java/Lab3_Part1.java
+++ 2022 Spring Java/Lab3_Part1.java
@@ -4,9 +4,12 @@
 
 import java.util.Scanner;
 
-public class Lab3_Part1 {
+public class Lab3_Part1 { 
     public static void main(String[] args) {
-        try (Scanner input = new Scanner(System.in)) {
+        // VS code suggested I close this with a try? I do not understand why
+        try (Scanner input = new Scanner(System.in)) { //instantiates a scanner input
+            
+            // Grabs 5 ints from user
             System.out.print("Enter a whole number: ");
             int num1 = input.nextInt();
 
@@ -22,18 +25,25 @@ public class Lab3_Part1 {
             System.out.print("Enter a whole number: ");
             int num5 = input.nextInt();
 
-            int numMax1 = Math.max(num1, num2);
-            int numMax2 = Math.max(num3, num4);
-            int numMax3 = Math.max(numMax1, numMax2);
-            int numMax4 = Math.max(numMax3, num5);
+            // using Java.Lang.Math methods
+            // I assume this lab is to help learn the fundamentals of operators and if statements
+            // and this kind of defeats that purpose.
+            // Let me know if you would like me to stick to what the lesson is trying to teach explicitly.
+            // For completeness, I know this method does take more momery (2 extra variables)
+            // It also may be slightly less efficient (it performs a NaN check and a -0 check first)
+            // but I find it much more readable and those cons have a negligible effet for such a small program
+            int numMax = Math.max(num1, num2);
+            numMax = Math.max(numMax, num3);
+            numMax = Math.max(numMax, num4);
+            numMax = Math.max(numMax, num5); // Returns max value
 
-            int numMin1 = Math.min(num1, num2);
-            int numMin2 = Math.min(num3, num4);
-            int numMin3 = Math.min(numMin1, numMin2);
-            int numMin4 = Math.min(numMin3, num5);
+            int numMin = Math.min(num1, num2);
+            numMin = Math.min(numMin, num3);
+            numMin = Math.min(numMin, num4);
+            numMin = Math.min(numMin, num5); // Returns min value
 
-            System.out.printf("The maximum number is: %d%n", numMax4);
-            System.out.printf("The minimum number is: %d", numMin4);
+            System.out.printf("The maximum number is: %d%n", numMax); // Displays max value
+            System.out.printf("The minimum number is: %d", numMin); // Displays min value
         }
 
     } // end method
